b4dc2cced4b69b993b9d63af001c190fb63da7ba
Lyudmila <milo4kamoya@gmail.com>
2021-10-19 20:19:22 -0700

    styles added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 let listItems = ["Buy Food", "Prepare Dinner"];
