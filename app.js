94b47fe94f1cc516b96149e0370c983272ab4ee7
Lyudmila <milo4kamoya@gmail.com>
2021-11-15 16:16:58 -0800

    Procfile added

--- app.js
+++ app.js
@@ -121,6 +121,6 @@ app.get("/:id", (req, res) => {
   })
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000.");
 });
